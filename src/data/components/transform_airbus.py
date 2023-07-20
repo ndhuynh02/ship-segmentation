a4555615b398574ff060bcbeca9dc9084fc74c4d
daoquyan2003 <daoquyan26122003@gmail.com>
2023-07-20 23:19:02 +0700

    Add Resize transformation

--- src/data/components/transform_airbus.py
+++ src/data/components/transform_airbus.py
@@ -12,39 +12,54 @@ from typing import Any, Optional
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class TransformAirbus(Dataset):
     mean = None
     std = None
 
-    def __init__(self, dataset: AirbusDataset, transform: Optional[Compose] = None) -> None:
+    def __init__(
+        self,
+        dataset: AirbusDataset,
+        transform: Optional[Compose] = None,
+        img_size: Optional[int] = None,
+    ) -> None:
         super().__init__()
 
         self.dataset = dataset
+        self.img_size = img_size
 
         if transform is not None:
             self.transform = transform
         else:
-            self.transform = Compose([
-                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ToTensorV2(),
-            ])
+            self.transform = Compose(
+                [
+                    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                    ToTensorV2(),
+                ]
+            )
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, index) -> Any:
-        image, mask = self.dataset[index] # (768, 768, 3), (768, 768)
+        image, mask = self.dataset[index]  # (768, 768, 3), (768, 768)
 
         # fig, (ax1, ax2) = plt.subplots(1, 2)
         # ax1.imshow(image)
         # ax2.imshow(mask)
         # plt.pause(5)
 
+        if self.img_size is not None:
+            # Resize the image and mask to the specified size
+            resize_transform = A.Resize(self.img_size, self.img_size)
+            resized = resize_transform(image=image, mask=mask)
+            image, mask = resized["image"], resized["mask"]
+
         if self.transform is not None:
             transformed = self.transform(image=image, mask=mask)
-            image = transformed['image'] # (3, 768, 768)
-            mask = transformed['mask'] # (768, 768), uint8
-            mask = mask.unsqueeze(0).float() # (1, 768, 768)
+            image = transformed["image"]  # (3, img_size, img_size)
+            mask = transformed["mask"]  # (img_size, img_size), uint8
+            mask = mask.unsqueeze(0).float()  # (1, img_size, img_size)
 
         return image, mask
 
