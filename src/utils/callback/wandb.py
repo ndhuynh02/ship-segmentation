ce30e25dce8c1496957f5bd053d0b8244a34f5e8
daoquyan2003 <daoquyan26122003@gmail.com>
2023-07-27 20:09:16 +0700

    Remove the use of hydra in wandb callback

--- src/utils/callback/wandb.py
+++ src/utils/callback/wandb.py
@@ -1,8 +1,5 @@
 import wandb
 
-import hydra
-from omegaconf import DictConfig, OmegaConf
-
 import torch
 from pytorch_lightning.callbacks import Callback
 from torchvision.utils import make_grid
@@ -32,10 +29,6 @@ class WandbCallback(Callback):
         data_path: str = "data/airbus",
         img_size: int = 384,
     ):
-        # this line allows to access init params with 'self.hparams' attribute
-        # also ensures init params will be stored in ckpt
-        self.save_hyperparameters(logger=False)
-
         self.four_first_preds = []
         self.four_first_targets = []
         self.four_first_batch = []
@@ -51,9 +44,7 @@ class WandbCallback(Callback):
         image_path = os.path.join(image_path, image_id)
         self.sample_image = np.array(Image.open(image_path).convert("RGB"))
         self.img = np.array(
-            Image.open(image_path)
-            .convert("RGB")
-            .resize((self.hparams.img_size, self.hparams.img_size))
+            Image.open(image_path).convert("RGB").resize((self.img_size, self.img_size))
         )
         dataframe = pd.read_csv(
             os.path.join(data_path, "train_ship_segmentations_v2.csv")
@@ -63,7 +54,7 @@ class WandbCallback(Callback):
 
         self.transform = Compose(
             [
-                A.Resize(self.hparams.img_size, self.hparams.img_size),
+                A.Resize(self.img_size, self.img_size),
                 A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 ToTensorV2(),
             ]
