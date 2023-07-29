1039526355d873df53c859dd687a326f3018e436
daoquyan2003 <daoquyan26122003@gmail.com>
2023-07-29 16:42:54 +0700

    Initialize ResNet34 from Classifier weights instead of torchvision.models weights

--- src/models/components/unet34.py
+++ src/models/components/unet34.py
@@ -2,32 +2,53 @@ import torch
 from torch.nn import Module
 import torch.nn.functional as F
 from torchvision.models import resnet34, ResNet34_Weights
+from src.models.components.resnet34 import ResNet34_Binary
+from src.models.classifier_module import ResNetLitModule
+from src.models.components.mixedloss import MixedLoss
 
 # Initialize the ResNet34 model with pretrained weights
-rn34 = resnet34(weights=ResNet34_Weights.DEFAULT)
+# rn34 = resnet34(weights=ResNet34_Weights.DEFAULT)
+
+ckpt_path = "./ckpt/Classifier-768.ckpt"
+model = ResNetLitModule.load_from_checkpoint(
+    checkpoint_path=ckpt_path,
+    net=ResNet34_Binary(),
+    criterion=torch.nn.BCEWithLogitsLoss(),
+).net
 
 # Cut out the Average Pooling layer and Fully Connected layer to get the feature extractor part
-rn34_feature_extractor = torch.nn.Sequential(*list(rn34.children())[:-2])
+# rn34_feature_extractor = torch.nn.Sequential(*list(rn34.children())[:-2])
+
+rn34_feature_extractor = torch.nn.Sequential(*list(model.rn.children())[:-2])
+
 
 class UNet_Up_Block(torch.nn.Module):
     def __init__(self, up_in, x_in, n_out):
         super().__init__()
-        up_out = x_out = n_out//2
-        self.x_conv  = torch.nn.Conv2d(x_in,  x_out,  1)
+        up_out = x_out = n_out // 2
+        self.x_conv = torch.nn.Conv2d(x_in, x_out, 1)
         self.tr_conv = torch.nn.ConvTranspose2d(up_in, up_out, 2, stride=2)
         self.bn = torch.nn.BatchNorm2d(n_out)
 
     def forward(self, up_p, x_p):
         up_p = self.tr_conv(up_p)
         x_p = self.x_conv(x_p)
-        cat_p = torch.cat([up_p,x_p], dim=1)
+        cat_p = torch.cat([up_p, x_p], dim=1)
         return self.bn(F.relu(cat_p))
 
-class SaveFeatures():
-    features=None
-    def __init__(self, m): self.hook = m.register_forward_hook(self.hook_fn)
-    def hook_fn(self, module, input, output): self.features = output
-    def remove(self): self.hook.remove()
+
+class SaveFeatures:
+    features = None
+
+    def __init__(self, m):
+        self.hook = m.register_forward_hook(self.hook_fn)
+
+    def hook_fn(self, module, input, output):
+        self.features = output
+
+    def remove(self):
+        self.hook.remove()
+
 
 class Unet34(torch.nn.Module):
     def __init__(self, rn=rn34_feature_extractor):
@@ -50,10 +71,13 @@ class Unet34(torch.nn.Module):
         return x
 
     def close(self):
-        for sf in self.sfs: sf.remove()
+        for sf in self.sfs:
+            sf.remove()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     x = torch.rand((1, 3, 256, 256))
     model = Unet34()
+    print(model(x).shape)
     print(model(x).min())  # 'torch.Size([1, 1, 256, 256])
     print(model(x).max())
