747318241da178f5eea211ab284d7278183a0a9b
thivux <hanchungluong@gmail.com>
2023-09-19 15:28:46 +0000

    make lovasz loss traceable

--- src/models/loss_function/lovasz_loss.py
+++ src/models/loss_function/lovasz_loss.py
@@ -3,13 +3,11 @@ License)"""
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.nn import BCEWithLogitsLoss
 
-try:
-    from itertools import ifilterfalse
-except ImportError:  # py3k
-    from itertools import filterfalse
+# --------------------------- HELPER FUNCTIONS ---------------------------
 
 
 def lovasz_grad(gt_sorted):
@@ -17,34 +15,22 @@ def lovasz_grad(gt_sorted):
 
     1 in paper
     """
-    p = len(gt_sorted)
+    p = gt_sorted.shape[0]
     gts = gt_sorted.sum()
     intersection = gts - gt_sorted.float().cumsum(0)
     union = gts + (1 - gt_sorted).float().cumsum(0)
     jaccard = 1.0 - intersection / union
-    if p > 1:  # cover 1-pixel case
-        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
     return jaccard
 
 
-# --------------------------- BINARY LOSSES ---------------------------
-
-
-def lovasz_hinge(logits, labels, per_image=True, ignore=None):
+def lovasz_hinge(logits, labels):
     r"""
     Binary Lovasz hinge loss
       logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
       labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
-      per_image: compute the loss per image instead of per batch
-      ignore: void class id
     """
-    if per_image:
-        loss = mean(
-            lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
-            for log, lab in zip(logits, labels)
-        )
-    else:
-        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels))
     return loss
 
 
@@ -53,32 +39,68 @@ def lovasz_hinge_flat(logits, labels):
     Binary Lovasz hinge loss
       logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
       labels: [P] Tensor, binary ground truth labels (0 or 1)
-      ignore: label to ignore
     """
-    if len(labels) == 0:
-        # only void pixels, the gradients should be 0
-        return logits.sum() * 0.0
-    signs = 2.0 * labels.float() - 1.0
+
+    signs = 2.0 * labels.float() - 1.0  # labels = 0, signs < 0; labels = 1, signs > 0
     errors = 1.0 - logits * signs
     errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
-    perm = perm.data
+    #     perm = perm.data -> fixed
     gt_sorted = labels[perm]
     grad = lovasz_grad(gt_sorted)
-    elu = nn.ELU()
-    loss = torch.dot(elu(errors_sorted) + 1, grad)
+    loss = torch.dot(F.elu(errors_sorted) + 1, grad)
     return loss
 
 
-def flatten_binary_scores(scores, labels, ignore=None):
-    """Flattens predictions in the batch (binary case) Remove labels equal to 'ignore'."""
+def flatten_binary_scores(scores, labels):
+    """Flattens predictions in the batch (binary case)."""
     scores = scores.view(-1)
     labels = labels.view(-1)
-    if ignore is None:
-        return scores, labels
-    valid = labels != ignore
-    vscores = scores[valid]
-    vlabels = labels[valid]
-    return vscores, vlabels
+    return scores, labels
+
+
+def binary_xloss(logits, labels):
+    r"""
+    Binary Cross entropy loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+    """
+    logits, labels = flatten_binary_scores(logits, labels)
+    loss = StableBCELoss()(logits, labels.float())
+    return loss
+
+
+# --------------------------- MODULES ---------------------------
+
+
+class LovaszLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, logit, labels):
+        return lovasz_hinge(logit, labels)
+
+
+class BCE_Lovasz(nn.Module):
+    def __init__(self, pos_weight: torch.FloatTensor = None):
+        super().__init__()
+        self.nll_loss = BCEWithLogitsLoss(pos_weight=pos_weight)
+
+    def update_pos_weight(self, pos_weight: torch.FloatTensor = None):
+        if pos_weight is not None:
+            self.nll_loss.pos_weight = pos_weight
+
+    def forward(self, logit, labels):
+        return lovasz_hinge(logit, labels) + self.nll_loss(logit, labels)
+
+
+class SBCE_Lovasz(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, logit, truth):
+        bce = binary_xloss(logit, truth)
+        lovasz = lovasz_hinge(logit, truth)
+        return bce + lovasz
 
 
 class StableBCELoss(torch.nn.modules.Module):
@@ -91,71 +113,19 @@ class StableBCELoss(torch.nn.modules.Module):
         return loss.mean()
 
 
-def binary_xloss(logits, labels, ignore=None):
-    r"""
-    Binary Cross entropy loss
-      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
-      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
-      ignore: void class id
-    """
-    logits, labels = flatten_binary_scores(logits, labels, ignore)
-    loss = StableBCELoss()(logits, labels.float())
-    return loss
+if __name__ == "__main__":
+    torch.manual_seed(7749)
+    x1, y1 = torch.rand((1, 1, 256, 256)), torch.rand((1, 1, 256, 256))
+    x2, y2 = torch.rand((1, 1, 768, 768)), torch.rand((1, 1, 768, 768))
 
+    lovasz = LovaszLoss()
 
-# --------------------------- HELPER FUNCTIONS ---------------------------
+    traced = torch.jit.trace(lovasz, (x1, y1))
 
+    # make sure traced output is the same as original lovasz output
+    assert np.allclose(traced(x1, y1), lovasz(x1, y1))
 
-def mean(loss, ignore_nan=False, empty=0):
-    """nanmean compatible with generators."""
-    loss = iter(loss)
-    if ignore_nan:
-        loss = ifilterfalse(np.isnan, loss)
-    try:
-        n = 1
-        acc = next(loss)
-    except StopIteration:
-        if empty == "raise":
-            raise ValueError("Empty mean")
-        return empty
-    for n, v in enumerate(loss, 2):
-        acc += v
-    if n == 1:
-        return acc
-    return acc / n
+    # make sure torchscripted traced with (x1, y1) does generalize to (x2, y2)
+    assert np.allclose(traced(x2, y2), lovasz(x2, y2))
 
-
-class LovaszLoss(nn.Module):
-    def __init__(self, per_image=False):
-        super().__init__()
-        self.per_image = per_image
-
-    def forward(self, logit, labels):
-        return lovasz_hinge(logit, labels, per_image=self.per_image)
-
-
-class BCE_Lovasz(nn.Module):
-    def __init__(self, pos_weight: torch.FloatTensor = None, per_image=False):
-        super().__init__()
-        self.nll_loss = BCEWithLogitsLoss(pos_weight=pos_weight)
-        self.per_image = per_image
-
-    def update_pos_weight(self, pos_weight: torch.FloatTensor = None):
-        if pos_weight is not None:
-            self.nll_loss.pos_weight = pos_weight
-
-    def forward(self, logit, labels):
-        return lovasz_hinge(logit, labels, per_image=self.per_image, ignore=None) + self.nll_loss(
-            logit, labels
-        )
-
-
-class SBCE_Lovasz(nn.Module):
-    def __init__(self, per_image=False):
-        super().__init__()
-        self.per_image = per_image
-
-    def forward(self, logit, truth):
-        bce = binary_xloss(logit, truth)
-        lovasz = lovasz_hinge(logit, truth, per_image=self.per_image)
-        return bce + lovasz
+    print("ok")
