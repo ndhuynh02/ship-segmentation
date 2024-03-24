4da2f1978608cf3e37e3a13d469beabc2a7009a7
ndhuynh02 <nguyendanghuynh1804@gmail.com>
2024-03-24 10:09:19 +0000

    add missing params to _init_

--- src/models/loss_function/focalloss.py
+++ src/models/loss_function/focalloss.py
@@ -3,13 +3,17 @@ from torchvision.ops import sigmoid_focal_loss
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=0.25, gamma=2):
+    def __init__(self, alpha=0.25, gamma=2, reduction="mean"):
+        assert alpha > 0 and alpha < 1
+        assert reduction in ['none', 'mean', 'sum']
+        
         super().__init__()
         self.alpha = alpha
         self.gamma = gamma
+        self.reduction = reduction
 
     def forward(self, input, target):
         loss = sigmoid_focal_loss(
-            inputs=input, targets=target, alpha=self.alpha, gamma=self.gamma, reduction="mean"
+            inputs=input, targets=target, alpha=self.alpha, gamma=self.gamma, reduction=self.reduction
         )
         return loss
