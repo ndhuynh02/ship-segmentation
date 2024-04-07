1e5eb34a23927ccdf7a59c4c1495782071909b34
ndhuynh02 <nguyendanghuynh1804@gmail.com>
2024-04-07 04:43:09 +0000

    add `update_weight` function

--- src/models/loss_function/lossbinary.py
+++ src/models/loss_function/lossbinary.py
@@ -6,11 +6,15 @@ from torch.nn import BCEWithLogitsLoss
 class LossBinary(nn.Module):
     """Implementation from  https://github.com/ternaus/robot-surgery-segmentation."""
 
-    def __init__(self, jaccard_weight=0, pos_weight: torch.FloatTensor = None):
+    def __init__(self, jaccard_weight=0, weight: torch.FloatTensor = None, pos_weight = None):
         super().__init__()
-        self.nll_loss = BCEWithLogitsLoss(pos_weight=pos_weight)
+        self.nll_loss = BCEWithLogitsLoss(weight=weight, pos_weight=pos_weight)
         self.jaccard_weight = jaccard_weight
 
+    def update_weight(self, weight: torch.FloatTensor = None):
+        if weight is not None:
+            self.nll_loss.weight = weight
+    
     def update_pos_weight(self, pos_weight: torch.FloatTensor = None):
         if pos_weight is not None:
             self.nll_loss.pos_weight = pos_weight
